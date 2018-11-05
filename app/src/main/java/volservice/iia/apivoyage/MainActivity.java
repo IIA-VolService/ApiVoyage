d71af23692e65c85ab17e9be5942dac0ff0e9a9d
Tristan <neveuxtristan@gmail.com>
2018-11-05 16:39:52 +0100

    prepare all visual content (menu + navbar)

--- app/src/main/java/volservice/iia/apivoyage/MainActivity.java
+++ app/src/main/java/volservice/iia/apivoyage/MainActivity.java
@@ -1,9 +1,6 @@
 package volservice.iia.apivoyage;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -71,15 +68,15 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_flight) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_hostel) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_rent_a_car) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_settings) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_contact) {
 
         }
 
