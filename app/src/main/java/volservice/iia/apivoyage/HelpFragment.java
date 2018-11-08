7663e44d4dabbb7bf13a85fd86fee70522f62cec
Tristan <neveuxtristan@gmail.com>
2018-11-08 13:53:48 +0100

    create help content

--- app/src/main/java/volservice/iia/apivoyage/HelpFragment.java
+++ app/src/main/java/volservice/iia/apivoyage/HelpFragment.java
@@ -8,12 +8,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class SettingsFragment extends Fragment {
+public class HelpFragment extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_settings, null);
+        return inflater.inflate(R.layout.fragment_help, null);
     }
 
     @Override
--- app/src/main/java/volservice/iia/apivoyage/MainActivity.java
+++ app/src/main/java/volservice/iia/apivoyage/MainActivity.java
@@ -9,7 +9,6 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -81,7 +80,7 @@ public class MainActivity extends AppCompatActivity
         } else if (id == R.id.nav_rent_a_car) {
             fragment = new RentACarFragment();
         } else if (id == R.id.nav_settings) {
-            fragment = new SettingsFragment();
+            fragment = new HelpFragment();
         } else if (id == R.id.nav_contact) {
             fragment = new ContactFragment();
         }
