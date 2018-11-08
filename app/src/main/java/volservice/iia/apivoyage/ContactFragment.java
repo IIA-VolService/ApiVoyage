4ea7eeb64e0d5464ae9c97253d852a325d1475fe
Tristan <neveuxtristan@gmail.com>
2018-11-08 10:41:39 +0100

    add contact page layout + comportement

--- app/src/main/java/volservice/iia/apivoyage/ContactFragment.java
+++ app/src/main/java/volservice/iia/apivoyage/ContactFragment.java
@@ -1,5 +1,7 @@
 package volservice.iia.apivoyage;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,6 +9,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 public class ContactFragment extends Fragment {
 
@@ -20,7 +23,36 @@ public class ContactFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        // Initialiser contenu XML de la page
+        view.findViewById(R.id.contactImgGit).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/IIA-VolService/ApiVoyage")));
+            }
+        });
 
+        view.findViewById(R.id.contactSendEmail).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendEmail();
+            }
+        });
+
+    }
+
+    protected void sendEmail() {
+        String[] TO = {"t.neveux.du.geniebre@iia-laval.fr"};
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+
+        emailIntent.setData(Uri.parse("mailto:"));
+        emailIntent.setType("text/plain");
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, TO);
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Your subject");
+        emailIntent.putExtra(Intent.EXTRA_TEXT, "Email message goes here");
+
+        try {
+            startActivity(Intent.createChooser(emailIntent, "Send mail..."));
+        } catch (android.content.ActivityNotFoundException ex) {
+            Toast.makeText(this.getContext(), "Vous n'avez pas d'application de mailing", Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/volservice/iia/apivoyage/MainActivity.java
+++ app/src/main/java/volservice/iia/apivoyage/MainActivity.java
@@ -1,6 +1,5 @@
 package volservice.iia.apivoyage;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
@@ -21,12 +20,12 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+                this, drawer, null, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
