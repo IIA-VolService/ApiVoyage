e5dce07002dd5725a985927358f94bfd7f30fab9
Tristan <neveuxtristan@gmail.com>
2018-11-09 10:03:55 +0100

    préparation des items voiture / hotel

--- app/src/main/java/volservice/iia/apivoyage/items/CarItem.java
+++ app/src/main/java/volservice/iia/apivoyage/items/CarItem.java
@@ -1,10 +1,24 @@
 package volservice.iia.apivoyage.items;
 
 import java.io.Serializable;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class CarItem implements Serializable {
 
     private static final long serialVersionUID = -2163051469151804394L;
 
+    private String modele;
+    private String dateDebut;
+    private String dateFin;
+    private String prix;
 
+    public CarItem(String modele, String prix, Date dateDebut, Date dateFin) {
+        this.modele = modele;
+        this.prix = prix;
+        DateFormat formatterDay = new SimpleDateFormat("dd/MM/yyyy");
+        this.dateDebut = formatterDay.format(dateDebut);
+        this.dateFin = formatterDay.format(dateFin);
+    }
 }
--- app/src/main/java/volservice/iia/apivoyage/items/HostelItem.java
+++ app/src/main/java/volservice/iia/apivoyage/items/HostelItem.java
@@ -1,9 +1,28 @@
 package volservice.iia.apivoyage.items;
 
 import java.io.Serializable;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class HostelItem implements Serializable {
 
     private static final long serialVersionUID = -2163051469151804394L;
 
+    private String nom;
+    private String nbEtoile;
+    private String tailleChambre;
+    private String dateDebut;
+    private String dateFin;
+    private String prix;
+
+    public HostelItem(String nom, String nbEtoile, String tailleChambre, String prix, Date dateDebut, Date dateFin) {
+        this.nom = nom;
+        this.nbEtoile = nbEtoile;
+        this.tailleChambre = tailleChambre;
+        this.prix = prix;
+        DateFormat formatterDay = new SimpleDateFormat("dd/MM/yyyy");
+        this.dateDebut = formatterDay.format(dateDebut);
+        this.dateFin = formatterDay.format(dateFin);
+    }
 }
