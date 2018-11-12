d1763d53a320887a826ae2ed52acbe871372986e
Tristan <neveuxtristan@gmail.com>
2018-11-12 11:44:04 +0100

    prepare api01 rena a car

--- app/src/main/java/volservice/iia/apivoyage/fragments/RentACarFragment.java
+++ app/src/main/java/volservice/iia/apivoyage/fragments/RentACarFragment.java
@@ -14,17 +14,26 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+
+import javax.net.ssl.HttpsURLConnection;
 
 import volservice.iia.apivoyage.R;
 import volservice.iia.apivoyage.fragments.resultsList.CarResultFragment;
 import volservice.iia.apivoyage.fragments.resultsList.HostelResultFragment;
 import volservice.iia.apivoyage.items.CarItem;
+import volservice.iia.apivoyage.items.EnumAPI;
 
 public class RentACarFragment extends Fragment {
 
     private EditText editTextVilleLocation;
-    private EditText editTextAgenceLocation;
     private EditText editTextDateDebutReservationLocation;
     private EditText editTextDateFinReservationLocation;
     private EditText editTextTypeVoiture;
@@ -33,8 +42,7 @@ public class RentACarFragment extends Fragment {
     private TextView editTextMessageErreur;
 
     private String villeLocation;
-    private String agenceLocation;
-    private String typeVoiture;
+    private String marqueVoiture;
     private String dateDebutReservationLocation;
     private String dateFinReservationLocation;
 
@@ -43,7 +51,7 @@ public class RentACarFragment extends Fragment {
 
 
     //requetes https Voiture
-    public boolean startRequest() throws IOException {
+    public boolean startRequest() throws IOException, JSONException {
 
         if (askCarsAPI01()) {
             if (askCarsAPI02()) {
@@ -59,12 +67,173 @@ public class RentACarFragment extends Fragment {
 
     }
 
-    private boolean askCarsAPI01() {
-        return false;
+    /**
+     * Tanguy et Mael
+     *
+     * @return bool
+     */
+    private boolean askCarsAPI01() throws IOException, JSONException {
+        String getListVoiture = "https://192.168.214.23:5001/api/VoitureReservation/marque/" + marqueVoiture;
+
+        URL url = new URL(getListVoiture);
+        HttpsURLConnection cnn = (HttpsURLConnection) url.openConnection();
+        cnn.setRequestMethod("GET");
+        InputStreamReader in = new InputStreamReader(cnn.getInputStream());
+
+        BufferedReader br = new BufferedReader(in);
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while ((line = br.readLine()) != null) {
+            sb.append(line + "\n");
+        }
+        br.close();
+
+        JSONArray jsonArray = new JSONArray(sb.toString());
+
+        try {
+            parseFlyJsonAPI01(jsonArray);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return lstAPI01 != null && lstAPI01.length > 0;
     }
 
-    private boolean askCarsAPI02() {
-        return false;
+    private void parseFlyJsonAPI01(JSONArray reader) throws JSONException {
+        CarItem item;
+
+        int size = reader.length();
+
+        CarItem[] listRes = new CarItem[size];
+
+        for (int i = 0; i < size; i++) {
+            item = new CarItem(
+                    i,
+                    EnumAPI.API01,
+                    reader.getJSONObject(i).getString("modele"),
+                    Integer.valueOf(reader.getJSONObject(i).getString("prix")),
+                    dateDebutReservationLocation,
+                    dateFinReservationLocation,
+                    Integer.valueOf(reader.getJSONObject(i).getString("nbPassager")),
+                    reader.getJSONObject(i).getString("localisation"),
+                    reader.getJSONObject(i).getString("marque")
+            );
+            listRes[i] = item;
+        }
+        lstAPI01 = listRes;
+    }
+
+
+    /**
+     * Camille et Robin
+     *
+     * @return bool
+     */
+    private boolean askCarsAPI02() throws JSONException, IOException {
+        int idMarque;
+        int nbCar;
+        ArrayList<Integer> idModel;
+        JSONArray jsonArray = new JSONArray(getSbForRequest("https://192.168.214.31/brandname/read.php?token=SZAdlqfwCdb18CzUads4tLTqN6EaLRlr").toString());
+
+        try {
+            idMarque = askForMarqueId(jsonArray);
+            if (idMarque != -1) {
+                jsonArray = new JSONArray(getSbForRequest("https://192.168.214.31/model/read.php?token=SZAdlqfwCdb18CzUads4tLTqN6EaLRlr").toString());
+                idModel = askForAllModels(jsonArray, idMarque);
+                if (idModel != null) {
+                    jsonArray = new JSONArray(getSbForRequest("https://192.168.214.31/brandname/read_one.php?token=SZAdlqfwCdb18CzUads4tLTqN6EaLRlr&idBrandName=" + idMarque).toString());
+                    nbCar = askForIdCar(jsonArray);
+                    if (nbCar > 0) {
+                        jsonArray = new JSONArray(getSbForRequest("https://192.168.214.31/car/read.php?token=SZAdlqfwCdb18CzUads4tLTqN6EaLRlr").toString());
+                        parseFlyJsonAPI02(jsonArray, idMarque, nbCar);
+
+                    } else {
+                        lstAPI02 = new CarItem[0];
+                        return true;
+                    }
+                } else {
+                    lstAPI02 = new CarItem[0];
+                    return true;
+                }
+            } else {
+                lstAPI02 = new CarItem[0];
+                return true;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return lstAPI02 != null;
+    }
+
+    private void parseFlyJsonAPI02(JSONArray reader, int idCar, int nbCar) throws JSONException {
+        CarItem item;
+
+        int size = reader.length();
+
+        CarItem[] listRes = new CarItem[nbCar];
+
+        for (int i = 0; i < size; i++) {
+                item = new CarItem(
+                        i,
+                        EnumAPI.API02,
+                        reader.getJSONObject(i).getString("modele"),
+                        Integer.valueOf(reader.getJSONObject(i).getString("prix")),
+                        dateDebutReservationLocation,
+                        dateFinReservationLocation,
+                        Integer.valueOf(reader.getJSONObject(i).getString("nbPassager")),
+                        reader.getJSONObject(i).getString("localisation"),
+                        reader.getJSONObject(i).getString("marque")
+                );
+            listRes[i] = item;
+        }
+        lstAPI01 = listRes;
+    }
+
+    private StringBuilder getSbForRequest(String s) throws IOException {
+        URL url = new URL(s);
+        HttpsURLConnection cnn = (HttpsURLConnection) url.openConnection();
+        cnn.setRequestMethod("GET");
+        InputStreamReader in = new InputStreamReader(cnn.getInputStream());
+
+        BufferedReader br = new BufferedReader(in);
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while ((line = br.readLine()) != null) {
+            sb.append(line + "\n");
+        }
+        br.close();
+        return sb;
+    }
+
+    private int askForMarqueId(JSONArray reader) throws JSONException {
+        int size = reader.length();
+        for (int i = 0; i < size; i++) {
+            if (reader.getJSONObject(i).getString("name").toLowerCase().trim().equals(marqueVoiture.toLowerCase().trim())) {
+                return reader.getJSONObject(i).getInt("id");
+            }
+        }
+        return -1;
+    }
+
+    private int askForIdCar(JSONArray reader) throws JSONException {
+        int size = reader.length();
+        for (int i = 0; i < size; i++) {
+            if (reader.getJSONObject(i).getString("name").toLowerCase().trim().equals(marqueVoiture.toLowerCase().trim())) {
+                return reader.getJSONObject(i).getInt("id");
+            }
+        }
+        return -1;
+    }
+
+    private ArrayList<Integer> askForAllModels(JSONArray reader, int idMarque) throws JSONException {
+        int size = reader.length();
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        for (int i = 0; i < size; i++) {
+            if (reader.getJSONObject(i).getInt("idBrandName") == idMarque) {
+                result.add(reader.getJSONObject(i).getInt("id"));
+            }
+        }
+        return result.isEmpty() ? null : result;
     }
 
     @Nullable
@@ -88,11 +257,10 @@ public class RentACarFragment extends Fragment {
         btnSearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                villeLocation = editTextVilleLocation.toString();
-                agenceLocation = editTextAgenceLocation.toString();
-                typeVoiture = editTextTypeVoiture.toString();
-                dateDebutReservationLocation = editTextDateDebutReservationLocation.toString();
-                dateFinReservationLocation = editTextDateFinReservationLocation.toString();
+                villeLocation = editTextVilleLocation.getText().toString();
+                marqueVoiture = editTextTypeVoiture.getText().toString();
+                dateDebutReservationLocation = editTextDateDebutReservationLocation.getText().toString();
+                dateFinReservationLocation = editTextDateFinReservationLocation.getText().toString();
 
                 if (checkIfFormIsCorrect()) {
                     editTextMessageErreur.setVisibility(View.INVISIBLE);
@@ -111,6 +279,8 @@ public class RentACarFragment extends Fragment {
                         }
                     } catch (IOException e) {
                         e.printStackTrace();
+                    } catch (JSONException e) {
+                        e.printStackTrace();
                     }
 
 
@@ -123,7 +293,7 @@ public class RentACarFragment extends Fragment {
     }
 
     private boolean checkIfFormIsCorrect() {
-        if (!villeLocation.isEmpty() && !agenceLocation.isEmpty() && !typeVoiture.isEmpty() && !dateDebutReservationLocation.isEmpty() && !dateFinReservationLocation.isEmpty()) {
+        if (!villeLocation.isEmpty() && !marqueVoiture.isEmpty() && !dateDebutReservationLocation.isEmpty() && !dateFinReservationLocation.isEmpty()) {
             return true;
         }
         editTextMessageErreur.setText(getText(R.string.txt_frg_flight_error_champs_invalides));
--- app/src/main/java/volservice/iia/apivoyage/items/CarItem.java
+++ app/src/main/java/volservice/iia/apivoyage/items/CarItem.java
@@ -2,6 +2,7 @@ package volservice.iia.apivoyage.items;
 
 import java.io.Serializable;
 import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -14,14 +15,14 @@ public class CarItem implements Serializable {
     private String dateFin;
     private String prix;
     private String nbPlaces;
-    private String loueur;
+    private String localisation;
     private String classe;
     private String marque;
     private int type;
     private int id;
     private EnumAPI api;
 
-    public CarItem(int id, EnumAPI enumAPI, String modele, int prix, Date dateDebut, Date dateFin, int nbPlaces, String loueur, String marque) {
+    public CarItem(int id, EnumAPI enumAPI, String modele, int prix, String dateDebut, String dateFin, int nbPlaces, String localisation, String marque) {
         this.id = id;
         this.api = enumAPI;
         this.modele = modele;
@@ -29,8 +30,16 @@ public class CarItem implements Serializable {
         this.classe = prix > 50 ? prix > 100 ? "PREMIMUM" : "AFFAIRE" : "ECONONIMQUE";
         this.type = prix > 50 ? prix > 100 ? 1 : 2 : 3;
         this.nbPlaces = String.valueOf(nbPlaces) + (nbPlaces > 1 ? "places" : "place");
-        this.loueur = loueur;
+        this.localisation = localisation;
         this.marque = marque;
+        Date dateArr = null;
+        Date dateRet = null;
+        try {
+            dateArr = new SimpleDateFormat("dd-MM-yyyy").parse(dateDebut);
+            dateRet = new SimpleDateFormat("dd-MM-yyyy").parse(dateFin);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
         DateFormat formatterDay = new SimpleDateFormat("dd/MM/yyyy");
         this.dateDebut = formatterDay.format(dateDebut);
         this.dateFin = formatterDay.format(dateFin);
